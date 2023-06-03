a9237c995ad784b75d690c7d0d19ef2c617c0196
EchoEdyP <forsocmedecho@proton.me>
2023-06-03 22:20:50 +0700

    change default value in user,pass,dbname

--- internal/models/config.go
+++ internal/models/config.go
@@ -2,9 +2,9 @@ package models
 
 type Config struct {
 	DBDriver string `envconfig:"DB_DRIVER" default:"postgres"`
-	DBUser   string `envconfig:"DB_USER" default:"postgres"`
-	DBPass   string `envconfig:"DB_PASS" default:"postgres"`
-	DBName   string `envconfig:"DB_NAME" default:"cli_wikis"`
+	DBUser   string `envconfig:"DB_USER" default:"eep"`
+	DBPass   string `envconfig:"DB_PASS" default:"1903"`
+	DBName   string `envconfig:"DB_NAME" default:"mydatabase"`
 	DBHost   string `envconfig:"DB_HOST" default:"localhost"`
 	DBPort   int    `envconfig:"DB_PORT" default:"5432"`
 }
